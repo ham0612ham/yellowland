b274cfc545ff2b2763251f2d50c308003a1bff0f
ham0612 <hyemangi@naver.com>
2022-12-15 17:18:31 +0900

    2022.12.15 안혜민 다시

--- src/main/java/com/sp/app/parse/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.sp.app.parse;
-
-import java.util.Map;
-
-public interface UserService {
-	public Map<String, Object> serializeNode(String spec);
-	public String documentWriter(String spec);
-}
