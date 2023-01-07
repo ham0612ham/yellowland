ff32384bb665af15f8aa92a8c79c227443a17e95
jang <jang@DESKTOP-1QOKUOI>
2023-01-07 17:41:55 +0900

    관리자 공지 관련

--- src/main/java/com/sp/app/admin/noticeManage/NoticeManageController.java
+++ src/main/java/com/sp/app/admin/noticeManage/NoticeManageController.java
@@ -1,25 +1,367 @@
 package com.sp.app.admin.noticeManage;
 
+import java.io.File;
+import java.io.PrintWriter;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller("notice.noticeManageController")
+import com.sp.app.common.FileManager;
+import com.sp.app.common.MyUtil;
+import com.sp.app.member.SessionInfo;
+
+@Controller("admin.noticeManage.noticeManageController")
 @RequestMapping("/admin/noticeManage/*")
 public class NoticeManageController {
-	@RequestMapping(value="list")
-	public String list (Model model)throws Exception{
-		return ".admin.notice.list";
-	}
-	
-	@RequestMapping(value="article")
-	public String article (Model model)throws Exception{
-		return ".admin.notice.article";
-	}
-	
-	@RequestMapping(value="write")
-	public String write (Model model)throws Exception{
-		return ".admin.notice.write";
+	@Autowired
+	private NoticeManageService service;
+	@Autowired
+	private MyUtil myUtil;
+	@Autowired
+	private FileManager fileManager;
+
+	@RequestMapping(value = "list")
+	public String list(@RequestParam(value = "page", defaultValue = "1") int current_page,
+			@RequestParam(defaultValue = "all") String condition,
+			@RequestParam(defaultValue = "") String keyword,
+			HttpServletRequest req,
+			Model model) throws Exception {
+
+		int size = 10; // 한 화면에 보여주는 게시물 수
+		int total_page = 0;
+		int dataCount = 0;
+
+		if (req.getMethod().equalsIgnoreCase("GET")) { // GET 방식인 경우
+			keyword = URLDecoder.decode(keyword, "utf-8");
+		}
+
+		// 전체 페이지 수
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("condition", condition);
+		map.put("keyword", keyword);
+
+		dataCount = service.dataCount(map);
+		if (dataCount != 0) {
+			total_page = myUtil.pageCount(dataCount, size);
+		}
+
+		// 다른 사람이 자료를 삭제하여 전체 페이지수가 변화 된 경우
+		if (total_page < current_page) {
+			current_page = total_page;
+		}
+
+		
+		
+
+		// 리스트에 출력할 데이터를 가져오기
+		int offset = (current_page - 1) * size;
+		if(offset < 0) offset = 0;
+
+		map.put("offset", offset);
+		map.put("size", size);
+
+		// 글 리스트
+		List<Notice> list = service.listNotice(map);
+
+		Date endDate = new Date();
+		long gap;
+		SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		for (Notice dto : list) {
+			Date beginDate = formatter.parse(dto.getReg_date());
+			/*
+			 // 날짜차이(일)
+			 gap=(endDate.getTime() - beginDate.getTime()) / (24 * 60 * 60 * 1000);
+			 dto.setGap(gap);
+			 */
+			// 날짜차이(시간)
+			gap = (endDate.getTime() - beginDate.getTime()) / (60 * 60 * 1000);
+			dto.setGap(gap);
+
+			dto.setReg_date(dto.getReg_date().substring(0, 10));
+		}
+
+		String cp = req.getContextPath();
+		String query = "";
+		String listUrl = cp + "/admin/noticeManage/list";
+		String articleUrl = cp + "/admin/noticeManage/article?page=" + current_page;
+		if (keyword.length() != 0) {
+			query = "condition=" + condition + "&keyword=" + URLEncoder.encode(keyword, "utf-8");
+		}
+
+		if (query.length() != 0) {
+			listUrl = cp + "/admin/noticeManage/list?" + query;
+			articleUrl = cp + "/admin/noticeManage/article?page=" + current_page + "&" + query;
+		}
+
+		String paging = myUtil.paging(current_page, total_page, listUrl);
+
+		
+		model.addAttribute("list", list);
+		model.addAttribute("page", current_page);
+		model.addAttribute("dataCount", dataCount);
+		model.addAttribute("size", size);
+		model.addAttribute("total_page", total_page);
+		model.addAttribute("paging", paging);
+		model.addAttribute("articleUrl", articleUrl);
+
+		model.addAttribute("condition", condition);
+		model.addAttribute("keyword", keyword);
+
+		return ".admin.noticeManage.list";
 	}
 
+	@RequestMapping(value = "write", method = RequestMethod.GET)
+	public String writeForm(Model model, HttpSession session) throws Exception {
+
+		SessionInfo info = (SessionInfo) session.getAttribute("member");
+		if (info.getMembership() < 51) {
+			return "redirect:/admin/noticeManage/list";
+		}
+
+		model.addAttribute("mode", "write");
+
+		return ".admin.noticeManage.write";
+	}
+
+	@RequestMapping(value = "write", method = RequestMethod.POST)
+	public String writeSubmit(Notice dto, HttpSession session) throws Exception {
+
+		SessionInfo info = (SessionInfo) session.getAttribute("member");
+
+		if (info.getMembership() < 51) {
+			return "redirect:/admin/noticeManage/list";
+		}
+
+		try {
+			String root = session.getServletContext().getRealPath("/");
+			String pathname = root + "uploads" + File.separator + "noticeManage";
+
+			dto.setUserId(info.getUserId());
+			service.insertNotice(dto, pathname);
+		} catch (Exception e) {
+		}
+
+		return "redirect:/admin/noticeManage/list";
+	}
+
+	@RequestMapping(value = "article")
+	public String article(@RequestParam long num,
+			@RequestParam String page,
+			@RequestParam(defaultValue = "all") String condition,
+			@RequestParam(defaultValue = "") String keyword,
+			Model model) throws Exception {
+
+		keyword = URLDecoder.decode(keyword, "utf-8");
+
+		String query = "page=" + page;
+		if (keyword.length() != 0) {
+			query += "&condition=" + condition + "&keyword=" + URLEncoder.encode(keyword, "UTF-8");
+		}
+
+		service.updateHitCount(num);
+
+		Notice dto = service.readNotice(num);
+		if (dto == null) {
+			return "redirect:/admin/noticeManage/list?" + query;
+		}
+
+		dto.setContent(dto.getContent().replaceAll("\n", "<br>"));
+
+		// 이전 글, 다음 글
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("condition", condition);
+		map.put("keyword", keyword);
+		map.put("num", num);
+
+		Notice preReadDto = service.preReadNotice(map);
+		Notice nextReadDto = service.nextReadNotice(map);
+
+		// 파일
+		List<Notice> listFile = service.listFile(num);
+
+		model.addAttribute("dto", dto);
+		model.addAttribute("preReadDto", preReadDto);
+		model.addAttribute("nextReadDto", nextReadDto);
+		model.addAttribute("listFile", listFile);
+		model.addAttribute("page", page);
+		model.addAttribute("query", query);
+
+		return ".admin.noticeManage.article";
+	}
+
+	@RequestMapping(value = "update", method = RequestMethod.GET)
+	public String updateForm(@RequestParam long num,
+			@RequestParam String page,
+			HttpSession session,
+			Model model) throws Exception {
+		SessionInfo info = (SessionInfo) session.getAttribute("member");
+
+		Notice dto = service.readNotice(num);
+		if (dto == null || ! info.getUserId().equals(dto.getUserId())) {
+			return "redirect:/admin/noticeManage/list?page=" + page;
+		}
+
+		List<Notice> listFile = service.listFile(num);
+
+		model.addAttribute("mode", "update");
+		model.addAttribute("page", page);
+		model.addAttribute("dto", dto);
+		model.addAttribute("listFile", listFile);
+
+		return ".admin.noticeManage.write";
+	}
+
+	@RequestMapping(value = "update", method = RequestMethod.POST)
+	public String updateSubmit(Notice dto,
+			@RequestParam String page,
+			HttpSession session) throws Exception {
+
+		SessionInfo info = (SessionInfo) session.getAttribute("member");
+		if (info.getMembership() < 51) {
+			return "redirect:/admin/noticeManage/list?page=" + page;
+		}
+
+		try {
+			String root = session.getServletContext().getRealPath("/");
+			String pathname = root + File.separator + "uploads" + File.separator + "noticeManage";
+
+			dto.setUserId(info.getUserId());
+			service.updateNotice(dto, pathname);
+		} catch (Exception e) {
+		}
+
+		return "redirect:/admin/noticeManage/list?page=" + page;
+	}
+
+	@RequestMapping(value = "delete")
+	public String delete(@RequestParam long num,
+			@RequestParam String page,
+			@RequestParam(defaultValue = "all") String condition,
+			@RequestParam(defaultValue = "") String keyword,
+			HttpSession session) throws Exception {
+		SessionInfo info = (SessionInfo) session.getAttribute("member");
+
+		keyword = URLDecoder.decode(keyword, "utf-8");
+		String query = "page=" + page;
+		if (keyword.length() != 0) {
+			query += "&condition=" + condition + "&keyword=" + URLEncoder.encode(keyword, "UTF-8");
+		}
+
+		if (info.getMembership() < 51) {
+			return "redirect:/admin/noticeManage/list?" + query;
+		}
+
+		try {
+			String root = session.getServletContext().getRealPath("/");
+			String pathname = root + "uploads" + File.separator + "noticeManage";
+			service.deleteNotice(num, pathname);
+		} catch (Exception e) {
+		}
+
+		return "redirect:/admin/noticeManage/list?" + query;
+	}
+
+	@RequestMapping(value = "download")
+	public void download(@RequestParam long fileNum,
+			HttpServletResponse resp,
+			HttpSession session) throws Exception {
+		String root = session.getServletContext().getRealPath("/");
+		String pathname = root + "uploads" + File.separator + "noticeManage";
+
+		boolean b = false;
+
+		Notice dto = service.readFile(fileNum);
+		if (dto != null) {
+			String saveFilename = dto.getSaveFilename();
+			String oriFilename = dto.getOriFilename();
+
+			b = fileManager.doFileDownload(saveFilename, oriFilename, pathname, resp);
+		}
+
+		if (! b) {
+			try {
+				resp.setContentType("text/html; charset=utf-8");
+				PrintWriter out = resp.getWriter();
+				out.println("<script>alert('파일 다운로드가 불가능 합니다 !!!');history.back();</script>");
+			} catch (Exception e) {
+			}
+		}
+	}
+
+	@RequestMapping(value = "zipdownload")
+	public void zipdownload(@RequestParam long num,
+			HttpServletResponse resp,
+			HttpSession session) throws Exception {
+		String root = session.getServletContext().getRealPath("/");
+		String pathname = root + "uploads" + File.separator + "noticeManage";
+
+		boolean b = false;
+
+		List<Notice> listFile = service.listFile(num);
+		if (listFile.size() > 0) {
+			String[] sources = new String[listFile.size()];
+			String[] originals = new String[listFile.size()];
+			String zipFilename = num + ".zip";
+
+			for (int idx = 0; idx < listFile.size(); idx++) {
+				sources[idx] = pathname + File.separator + listFile.get(idx).getSaveFilename();
+				originals[idx] = File.separator + listFile.get(idx).getOriFilename();
+			}
+
+			b = fileManager.doZipFileDownload(sources, originals, zipFilename, resp);
+		}
+
+		if (! b) {
+			try {
+				resp.setContentType("text/html; charset=utf-8");
+				PrintWriter out = resp.getWriter();
+				out.println("<script>alert('파일 다운로드가 불가능 합니다 !!!');history.back();</script>");
+			} catch (Exception e) {
+			}
+		}
+	}
+
+	@RequestMapping(value = "deleteFile", method = RequestMethod.POST)
+	@ResponseBody
+	public Map<String, Object> deleteFile(@RequestParam long fileNum, HttpSession session) throws Exception {
+
+		String root = session.getServletContext().getRealPath("/");
+		String pathname = root + "uploads" + File.separator + "noticeManage";
+
+		Notice dto = service.readFile(fileNum);
+		if (dto != null) {
+			fileManager.doFileDelete(dto.getSaveFilename(), pathname);
+		}
+
+		String state = "false";
+		try {
+			Map<String, Object> map = new HashMap<String, Object>();
+			map.put("field", "fileNum");
+			map.put("num", fileNum);
+			service.deleteFile(map);
+			state = "true";
+		} catch (Exception e) {
+		}
+
+		// 작업 결과를 json으로 전송
+		Map<String, Object> model = new HashMap<>();
+		model.put("state", state);
+		return model;
+	}
 }
--- src/main/webapp/WEB-INF/views/admin/layout/header.jsp
+++ src/main/webapp/WEB-INF/views/admin/layout/header.jsp
@@ -11,36 +11,39 @@
 				<li><a class="navbar-brand" href="${pageContext.request.contextPath}/" style="padding-left: 0px"><img id="img-logo" src="${pageContext.request.contextPath}/resources/images/logo_white.png"></a></li>
 			</ul>
 			<ul class="main-menu2">
-				<li><a href="#" class="main-menu2-a">뜨는 상권</a></li>
-				<li><a href="#" class="main-menu2-a">상권 매칭</a></li>
-				<li><a href="#" class="main-menu2-a">상권 현황</a>
+				<li><a href="${pageContext.request.contextPath}/commercial/map" class="main-menu2-a">뜨는 상권</a></li>
+				<li><a href="${pageContext.request.contextPath}/matching/main" class="main-menu2-a">상권 매칭</a></li>
+				<li><a href="${pageContext.request.contextPath}/analysis/main" class="main-menu2-a">상권 현황</a>
 					<ul class="sub-menu2">
 						<li><a href="#" aria-label="subemnu">지역별 현황</a></li>
 						<li><a href="#" aria-label="subemnu">상권별 현황</a></li>
 					</ul>
 				</li>
-				<li><a href="#" class="main-menu2-a">커뮤니티</a>
+				<li><a href="${pageContext.request.contextPath}/assignComm/main" class="main-menu-a">상가 양도</a></li>
+				<li><a href="">커뮤니티</a>
 					<ul class="sub-menu2">
-						<li><a href="#" aria-label="subemnu">지역별 커뮤니티</a></li>
-						<li><a href="#" aria-label="subemnu">업종별 커뮤니티</a></li>
+						<li><a href="${pageContext.request.contextPath}/localCommunity/list" aria-label="subemnu">지역별 커뮤니티</a></li>
+						<li><a href="${pageContext.request.contextPath}/typeCommunity/list" aria-label="subemnu">업종별 커뮤니티</a></li>
 					</ul>
 				</li>
-				<li><a href="#" class="main-menu2-a">고객센터</a>
+				<li><a href="${pageContext.request.contextPath}/notice/list" class="main-menu2-a">고객센터</a>
 					<ul class="sub-menu2">
-						<li><a href="#" aria-label="subemnu">공지사항</a></li>
-						<li><a href="#" aria-label="subemnu">자주하는 질문</a></li>
-						<li><a href="#" aria-label="subemnu">1:1문의</a></li>
+						<li><a href="${pageContext.request.contextPath}/notice/list" aria-label="subemnu">공지사항</a></li>
+						<li><a href="${pageContext.request.contextPath}/faq/list" aria-label="subemnu">자주하는 질문</a></li>
+						<li><a href="${pageContext.request.contextPath}/qna/list" aria-label="subemnu">1:1문의</a></li>
 					</ul>
 				</li>
-				<li><a href="#" class="main-menu2-a">관리자 메뉴</a>
-					<ul class="sub-menu2">
-						<li><a href="#" aria-label="subemnu">공지사항 관리</a></li>
-						<li><a href="#" aria-label="subemnu">자주하는 질문 관리</a></li>
-						<li><a href="#" aria-label="subemnu">1:1문의 관리</a></li>
-						<li><a href="#" aria-label="subemnu">게시글 관리</a></li>
-						<li><a href="#" aria-label="subemnu">회원 관리</a></li>
-					</ul>
-				</li>
+				<c:if test="${sessionScope.member.membership==99}">
+					<li><a href="${pageContext.request.contextPath}/admin/noticeManage/list" class="main-menu2-a">관리자 메뉴</a>
+						<ul class="sub-menu2">
+							<li><a href="${pageContext.request.contextPath}/admin/noticeManage/list" aria-label="subemnu">공지사항 관리</a></li>
+							<li><a href="${pageContext.request.contextPath}/admin/faqManage/list" aria-label="subemnu">자주하는질문관리</a></li>
+							<li><a href="${pageContext.request.contextPath}/admin/qnaManage/list" aria-label="subemnu">1:1문의 관리</a></li>
+							<li><a href="${pageContext.request.contextPath}/admin/board/list" aria-label="subemnu">게시글 관리</a></li>
+							<li><a href="${pageContext.request.contextPath}/admin/memberManage/list" aria-label="subemnu">회원 관리</a></li>
+						</ul>
+					</li>
+				</c:if>
 			</ul>
 			</div>
 			<div class="d-flex justify-content-end login-menu">
